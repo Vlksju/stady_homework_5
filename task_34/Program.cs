2132b8f18d5b8392ec2098df504e23b15f619b51
Vlksju <xenya.vol2017@yandex.ru>
2022-09-17 20:15:04 +0500

    Solved task 34

---
+++ task_34/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Программа, которая ищет количество четных элементов случайнозаполненного массива трехзначными  числами");
+
+int[] arr = new int[7];
+Random rand = new Random();
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rand.Next(100, 1000);
+}
+
+Console.Write("Исходный массив = [");
+Console.Write(string.Join("] [", arr));
+Console.WriteLine("]");
+
+int sum = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i]%2==0) { sum++; }
+}
+
+Console.WriteLine($"Количество элементов = {sum}"); 
