9d2035253efe6914ba11a4d619939fa97a67c440
Vlksju <xenya.vol2017@yandex.ru>
2022-09-17 20:24:19 +0500

    Solved task 36

---
+++ task_36/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Программа, которая подсчитывает сумму элементов массива, стоящих на нечетных позициях");
+
+int[] arr = new int[6];
+Random rand = new Random();
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rand.Next(34, 736);
+}
+
+Console.Write("Исходный массив = [");
+Console.Write(string.Join("] [", arr));
+Console.WriteLine("]");
+
+int sum = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (i%2==1) { sum=sum+arr[i]; }
+}
+
+Console.WriteLine($"Сумма элементов = {sum}"); 
